523d20d321cb2530ba157f438e3efe341aaef98c
Aurelia <aureliarnj@gmail.com>
2023-09-16 18:54:31 +0700

    minor update

--- app.js
+++ app.js
@@ -7,20 +7,18 @@ const host = 'localhost';
 // setting views
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    const table = [
-        { row: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] },
-        { row: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'] },
-        { row: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'] },
-        { row: ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'] }
-    ];
+const table = [
+    { row: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] },
+    { row: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'] },
+    { row: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'] },
+    { row: ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'] }
+];
 
+app.get('/', (req, res) => {
     res.render('index', { table });
 });
 
-
-
-// Server
+// Connection
 app.listen(port, () => {
     console.log(`Server running on port http://${host}:${port}`);
 });
